bf2d4e4837ef744f063610fb74d47b028e848d12
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-08-08 15:21:53 +0530

    dsa 1

---
+++ Python/o_of_linera.py
@@ -0,0 +1,17 @@
+def find_paper(papers, name):
+    for paper in papers:
+        if paper == name:
+            return True
+    return False
+
+papers = ["Anita", "Babita", "Cat", "Doom", "Esha"]
+
+search_name = "Cat"
+
+result = find_paper(papers, search_name)
+
+if result:
+    print("Paper found")
+
+else:
+    print("paper not found.")
